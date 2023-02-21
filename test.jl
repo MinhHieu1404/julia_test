2dd6b21bc4c8901b11eecd798a8a97048a4e8154
minhhhieuuu <144020201hv@gmail.com>
2023-02-21 09:18:50 +0700

    add test.js file

---
+++ test.jl
@@ -0,0 +1,16 @@
+
+# Julia program to illustrate
+# the use of Arrays
+ 
+# Creating a 1D array
+Array1 = [1, 2, 3, 4]
+println(Array1)
+ 
+# Creating a 2D array
+Array2 = [1 2 3; 4 5 6]
+println(Array2)
+ 
+# Creating a 3D array
+# using 'cat' command
+Array3 =[1 2; 3 4], [5 6; 7 8], [2 2; 3 4]
+println(Array3)
